f50636f9b75a6c8a6bda519cfa5ce6c986b8ef0e
Jimmy <bruno_pier@hotmail.com>
2020-01-21 15:24:49 -0300

    Updating source code - reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,19 +2,19 @@ import * as actionTypes from './actions';
 
 const initialState = {
     persons: []
-}
+};
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
         case actionTypes.ADD_PERSON:
             const newPerson = {
                 id: Math.random(), // not really unique but good enough here!
-                name: 'Max',
+                name: 'Jimmy',
                 age: Math.floor( Math.random() * 40 )
             }
             return {
                 ...state,
-                persons: state.persons.concat(newPerson)
+                persons: state.persons.concat( newPerson )
             }
         case actionTypes.REMOVE_PERSON:
             return {
