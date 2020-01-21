c50f70970202240e0a6fec0bab42a0b8fe0d8bc0
Jimmy <bruno_pier@hotmail.com>
2020-01-21 15:33:05 -0300

    Adding class component in AddPerson.js - Replaceing original code

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends Component {
+    state = {
+        
+    }
+    render(){
+        return(
+            <div className="AddPerson">
+                <input type="text" placeholder="Name"/>
+                <input type="number" placeholder="Age"/>
+                <button onClick={this.props.personAdded}>Add Person</button>
+            </div>
+        );
+    }
+}
 
 export default addPerson;
