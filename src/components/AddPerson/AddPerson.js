34fef6121938b5e03419e7af74b1c5b75ac81049
Jimmy <bruno_pier@hotmail.com>
2020-01-21 15:37:10 -0300

    Binding entries - input - AddPerson.js

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -4,12 +4,27 @@ import './AddPerson.css';
 
 class AddPerson extends Component {
     state = {
-        
+        name: '',
+        age: null
     }
+
+    nameChangedHandler = (event) => {
+        this.setStateState({name: event.target.value});
+    }
+
+    ageChangedHandler = (event) => {
+        this.setStateState({age: event.target.value});
+    }
+
     render(){
         return(
             <div className="AddPerson">
-                <input type="text" placeholder="Name"/>
+                <input
+                type="text"
+                placeholder="Name"
+                onChange={this.nameChangedHandler}
+                value={}
+                />
                 <input type="number" placeholder="Age"/>
                 <button onClick={this.props.personAdded}>Add Person</button>
             </div>
