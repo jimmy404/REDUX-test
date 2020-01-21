fd2cf121b74269705d7befdda5ee8554233e0a07
Jimmy <bruno_pier@hotmail.com>
2020-01-21 14:23:17 -0300

    Adding import - reducer.js

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    persons: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+    }
+};
+
+export default reducer;
